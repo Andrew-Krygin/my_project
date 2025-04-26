0f7f88f5298c8d4b76c86c154cffab528b08026a
Andrew Krygin <andrew_krygin@protonmail.com>
2025-04-26 15:44:12 +0300

    Added sort_by_date function.

--- src/processing.py
+++ src/processing.py
@@ -6,3 +6,12 @@ def filter_by_state(records: list[dict[str, int | str]], state: str = "EXECUTED"
     """
     selected_records = [record for record in records if record.get("state") == state]
     return selected_records
+
+
+def sort_by_date(records: list[dict[str, int | str]], decreasing: bool = True) -> list[dict[str, int | str]]:
+    """
+    Функция принимает список словарей и необязательный параметр, задающий порядок
+    сортировки (по умолчанию — убывание). А затем возвращает новый список, отсортированный по дате (date).
+    """
+    sorted_records_of_date = sorted(records, key=lambda record: record.get("date", ""), reverse=decreasing)
+    return sorted_records_of_date
