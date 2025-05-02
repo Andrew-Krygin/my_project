72f2f3e7dd9166597a7f18f4395924693475d29c
Andrew Krygin <andrew_krygin@protonmail.com>
2025-05-02 16:55:02 +0300

    Fixtures used in the test_processing file have been added to the conftest file.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,39 @@ import pytest
 
 from src.masks import LENGTH_ACCOUNT_NUM, LENGTH_CARD_NUM
 
+
+@pytest.fixture
+def sample_data():
+    return [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 41428829, 'state': 'EXECUTED'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 615414851, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    ]
+
+
+@pytest.fixture
+def empty_data():
+    return [{}, {}, {},]
+
+
+@pytest.fixture
+def invalid_type_data():
+    return [1234, ("aededa",), set()]
+
+
+@pytest.fixture
+def invalid_date_data():
+    return [
+        {'id': 41428829, 'state': 'EXECUTED', "date": None},
+        {'id': 41428829, 'state': 'EXECUTED', "date": 1234},
+        {'id': 41428829, 'state': 'CANCELED', "date": "не_дата"},
+        {'id': 41428829, 'state': 'CANCELED'},
+    ]
+
+
 # Сообщения об ошибки для модуля test_widget.
 ERROR_MESSAGES = {
     "missing_data": """Недостаточно данных: должен быть тип и номер карты/счета.
