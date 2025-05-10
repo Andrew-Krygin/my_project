01ae867dcc61e02334f8378653421957c37bd631
Andrew Krygin <andrew_krygin@protonmail.com>
2025-05-10 21:33:53 +0300

    Update conftest file.Silenced linter warnings for fixture imports in conftest.py using # noqa: F401.

---
+++ tests/conftest.py
@@ -0,0 +1,3 @@
+from tests.fixtures.data_for_processing import (empty_data, invalid_date_data, invalid_type_data,  # noqa: F401
+                                                sample_data)
+from tests.fixtures.transactions import sample_transactions  # noqa: F401
